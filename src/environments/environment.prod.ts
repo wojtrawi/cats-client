310f49d22254759baa4dc363d97c67f2abf5024e
wojtrawi <wojtrawi@gmail.com>
2019-11-24 19:43:35 +0100

    Change api url for prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:7000',
+  apiUrl: 'https://cats-api-wt.herokuapp.com',
   auth0Domain: 'dev-43wokkyw.eu.auth0.com',
   auth0ClientId: 'znxv20jqUFK8MD7sRyRiQ7FNJUqD8udP',
 };
